3756afe5f0b8a3dce484e772d4b2b14fd746cd2f
srinathkv <srinath.crre@gmail.com>
2020-01-10 21:14:35 +0530

    Recursive

--- Test2/closestPairPoints.cpp
+++ Test2/closestPairPoints.cpp
@@ -46,17 +46,69 @@ float bruteForce(Point* a, int size)
 	return min;
 }
 
+float stripClosest(Point *strip,int  size,float  d)
+{
+	float min = d;
+	qsort(strip, size, sizeof(Point), compareY);
+
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = i + 1; j < size && (strip[j].y - strip[i].y) < min; ++j)
+		{
+			if (distance(strip[i], strip[j]) < min)
+			{
+				min = distance(strip[i], strip[j]);
+			}
+		}
+	}				
+	return min;
+}
+
+float closestRec(Point* a, int size)
+{
+	if (size <= 3)
+	{
+		return bruteForce(a, size);
+	}
+
+	int mid = size / 2;
+	Point midP = a[mid];
+
+	float dL = closestRec(a, mid);
+	float dR = closestRec(a + mid, size - mid);
+
+	float d = dL < dR ? dL : dR;
+
+	Point *strip = new Point[size];
+
+	int j = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		if (abs(a[i].x - midP.x) < d)
+		{
+			strip[j].x = a[i].x;
+			strip[j].y = a[i].y;
+			j++;
+		}
+	}
+
+	float dS =  stripClosest(strip, j,d);
+	return d < dS ? d : dS;
+
+}
+
 float closestPair(Point* a, int size)
 {
-	return bruteForce(a, size);
+	qsort(a, size, sizeof(Point), compareX);
+	return closestRec(a, size);
 }
 
 int main()
 {
 	Point a[] = { {2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4} };
 
-	qsort(a, 6,sizeof(Point), compareX);
+	
 
-	cout << " Min : " << bruteForce(a, sizeof(a) / sizeof(a[0]));
+	cout << " Min : " << closestPair(a, sizeof(a) / sizeof(a[0]));
 	return 0;
 }
